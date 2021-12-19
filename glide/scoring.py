b4e5a5013dbbe4e1f24d513345aae3c0198e56e8
kap-devkota <kapil.devkota@tufts.edu>
2021-12-19 16:22:49 -0500

    Removed debugging code from scoring

--- glide/scoring.py
+++ glide/scoring.py
@@ -6,65 +6,32 @@ from goatools.semantic import TermCounts, get_info_content
 from goatools.semantic import resnik_sim
 import pkg_resources
 
-def score_cv(test_nodes, test_labelling, real_labelling, debug_params = {"DEBUG" : False}):
+def score_cv(test_nodes, test_labelling, real_labelling):
     """Scores cross validation by counting the number of test nodes that
     were accurately labeled after their removal from the true
     labelling.
     """
     correct = 0
     total   = 0
-    if debug_params["DEBUG"] == True:
-        if debug_params["D_TYPE"] == "degree_check":
-            degree_vec   = debug_params["degree_vec"]
-            degree_thres = debug_params["degree_check"]["threshold"]
-            n_below      = 0
-            n_above      = 0
-            c_below      = 0
-            c_above      = 0
     for node in test_nodes:
         if node not in test_labelling:
             continue
-        if debug_params["DEBUG"] == True:
-            if debug_params["D_TYPE"] == "degree_check":
-                if degree_vec[node] >= degree_thres:
-                    n_above += 1
-                else:
-                    n_below += 1
         test_label = test_labelling[node]
         if type(test_label) is list:
             for tl in test_label:
                 if tl in real_labelling[node]:
-                    if debug_params["DEBUG"] == True:
-                        if debug_params["D_TYPE"] == "degree_check":
-                            if degree_vec[node] >= degree_thres:
-                                c_above += 1
-                            else:
-                                c_below += 1
                     correct += 1
                     break
         else:
             if test_label in real_labelling[node]:
-                if debug_params["DEBUG"] == True:
-                    if debug_params["D_TYPE"] == "degree_check":
-                        if degree_vec[node] >= degree_thres:
-                            c_above += 1
-                        else:
-                            c_below += 1
                 correct += 1
         total += 1
-    if debug_params["DEBUG"] == True:
-        if debug_params["D_TYPE"] == "degree_check":
-            b_acc = 0 if n_below == 0 else c_below / n_below
-            a_acc = 0 if n_above == 0 else c_above / n_above
-            print(f"[!] Error rate for degree below {degree_thres}: {c_below}, {n_below}, {b_acc}")
-            print(f"[!] Error rate for degree above {degree_thres}: {c_above}, {n_above}, {a_acc}")
     return float(correct) / float(total)
 
 def kfoldcv(k, 
             labels, 
             prediction_algorithm, 
             randomized=True, 
-            degree_vec = None, 
             reverse = False):
     """Performs k-fold cross validation.
 
@@ -78,10 +45,6 @@ def kfoldcv(k,
       - A list where each element is the accuracy of the
       learning algorithm holding out one fold of the data.
     """
-    debug_params = {"DEBUG"        : True if degree_vec is not None else False,
-                    "D_TYPE"       : "degree_check",
-                    "degree_vec"   : degree_vec,
-                    "degree_check" : {"threshold" : 10}}
     nodes = list(labels.keys())
     if randomized:
         random.shuffle(nodes)
@@ -104,8 +67,7 @@ def kfoldcv(k,
         
         accuracy = score_cv(test_nodes, 
                             test_labelling, 
-                            labels, 
-                            debug_params = debug_params)
+                            labels)
         accuracies.append(accuracy)
     return accuracies
 
